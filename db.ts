b22fccf78a021b771d4ae34b1f608dc4f7875a1b
Facundo Rodriguez <fr8080342@gmail.com>
2025-02-19 13:35:43 -0300

    console log

--- db.ts
+++ db.ts
@@ -1,5 +1,6 @@
 import admin from 'firebase-admin';
 
+console.log(process.env.SERVICE_ACCOUNT);
 let serviceAccount = JSON.parse(process.env.SERVICE_ACCOUNT!);
 admin.initializeApp ( { credential: admin.credential.cert(serviceAccount as any), databaseURL: 'https://apx-chat-9e04b-default-rtdb.firebaseio.com/' } );
 let firestore = admin.firestore();
